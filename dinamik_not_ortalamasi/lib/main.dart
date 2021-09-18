92ed7f262600ca17120775acaf619100e7eab343
Erkut <74938334+erkuttbulutt@users.noreply.github.com>
2021-09-18 20:48:55 +0300

    Dinamik-Not-Ortalaması

---
+++ dinamik_not_ortalamasi/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:dinamik_not_ortalamasi/constants/app_constants.dart';
+import 'package:dinamik_not_ortalamasi/widgets/ortalama_hesapla_page.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Dinamik Ortalama Hesapla',
+      theme: ThemeData(
+        primarySwatch: Sabitler.anaRenk,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: OrtalamaHesaplaPage(),
+    );
+  }
+}
